b280efec37ba24795fc8188027676cc26de6eb89
Никита <nikitakado.dev@gmail.com>
2023-09-20 13:44:43 +0300

    Initial commit

---
+++ src/main/java/com/kado/kpgateway/KpGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.kado.kpgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KpGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kado/kpgateway/KpGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kado.kpgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
